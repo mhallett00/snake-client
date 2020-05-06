9cfa1fb5e027787c0b207520ac9082286bd44e8f
Michael Hallett <m.hallett@gmail.com>
2020-05-06 18:27:25 +0000

    Offloaded user input from play.js file to input.js

---
+++ input.js
@@ -0,0 +1,21 @@
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin. setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume()
+  handleUserInput()
+  return stdin;
+};
+
+const handleUserInput = () => {
+
+  process.stdin.on('data', (key) => {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.stdout.write('\n');
+      process.exit();
+    }
+  });
+}
+
+module.exports = { setupInput }
